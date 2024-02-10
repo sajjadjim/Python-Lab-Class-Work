4b46df5d22e8652831b4b42806afef9ffa9eea9a
SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2024-02-10 15:37:21 +0600

    Create List &tupple.py

---
+++ List &tupple.py
@@ -0,0 +1,21 @@
+list_print = [111,222,333,'jim',3.56,9.899]
+
+for i in list_print:
+    print(i)
+
+
+print("The length of list :",len(list_print))  # length of the List
+print(type(list_print))
+
+print("Specefic print item :",list_print[0])
+print("Specefic range items print :",list_print[2:5])
+
+print("First to specific Lst print items :",list_print[ :3])
+print("Specific point to Last print items :",list_print[2: ])
+
+#Find the items exicts here :
+if 11 in list_print :
+    print("Yes !! it , stay here")
+
+else:
+    print("N0!!, its not exits here.")
