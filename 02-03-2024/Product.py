4801de1cc81641245c879cdef605141cf1e6d65f
SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2024-03-02 15:51:01 +0600

    Update Product.py

--- 02-03-2024/Product.py
+++ 02-03-2024/Product.py
@@ -1,10 +1,29 @@
+import tkinter as  tk
+
 product_name =[]
 product_price =[]
 product_quantity =[]
+product_discount=[]
 
 product_name.append("Bananna")
-product_price(10)
-product_quantity(2)
+product_price.append(10)
+product_quantity.append(2)
+product_discount.append(10)
+
+product_name.append("Milkshake")
+product_price.append(28)
+product_quantity.append(2)
+product_discount.append(3)
+
+product_name.append("Kitkat")
+product_price.append(70)
+product_quantity.append(2)
+product_discount.append(1)
+
+product_name.append("Flower")
+product_price.append(150)
+product_quantity.append(2)
+product_discount.append(3)
 
 x =len(product_name)
 sum =0
@@ -12,10 +31,11 @@ sum =0
 for i in range(x):
     price =int(product_price.pop())
     quality =float(product_quantity.pop())
-    #cal_price =price*(dis/10)
+    dis =int(product_discount.pop())
+    cal_price =price*(dis/10)
     print(product_name.pop() + "--" +str(price) + "--" +str(quality) +"--")
     sum =sum + price * quality
 
+print("----------!!!! Totall Price !!!!---------------")
 print(sum)
-
 
